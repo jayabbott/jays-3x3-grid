877af5f0e872d316f6d4ac0250ba0d502f3d04ec
Jay Abbott <jayabbott94@gmail.com>
2020-01-14 10:36:46 +0000

    Completed quiz

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -9,16 +9,18 @@
                 {{ questionIndex + 1 }}. {{ quizItem.question }}
                 <div class="control columns is-desktop">
                 <div class="column is-one-third-desktop is-one-third-widescreen is-one-third-fullhd answer-options" v-for="(answer, answerIndex) in quizItem.answers" v-bind:key="answerIndex">
-                    <input type="radio" v-bind:name="'q' + questionIndex + 'radios'" v-bind:id="'q' + questionIndex + 'a' + answerIndex" v-bind:value="'q' + answerIndex + 'a' + answerIndex" v-model="picked">
+                    <input type="radio" v-bind:name="'q' + questionIndex + 'radios'" v-bind:id="'q' + questionIndex + 'a' + answerIndex" v-bind:value="'q' + questionIndex + 'a' + answerIndex" v-model="quizItem.chosenAnswer">
                     <label class="answer-label" v-bind:for="'q' + questionIndex + 'a' + answerIndex">{{ answer }}</label>
                 </div>
                 </div>
             </li>
         </ol>
         <div class="button-container lick">
-            <button class="button is-primary" id="submit" @click="submit">Submit</button>
+            <button class="button is-primary" id="submit" @click="checkAnswers">Submit</button>
         </div>
+
     </div>
+
 </template>
 
 <script>
@@ -31,7 +33,8 @@ var quiz = [
       'Amore',
       'An old song',
     ],
-    correctAnswer: 1
+    correctAnswer: 1,
+    chosenAnswer: null,
   },
   {
     question: 'A thing is moving away from you and you bouce a signal (such as sound, light or a radio signal) off it. What happens to your perception of the frequency of the singal you get back?',
@@ -40,7 +43,8 @@ var quiz = [
       'It gets higher',
       'It gets lower',
     ],
-    correctAnswer: 2
+    correctAnswer: 2,
+    chosenAnswer: null,
   },
   {
     question: 'How many corners does a cube have?',
@@ -49,7 +53,8 @@ var quiz = [
       '8',
       '10',
     ],
-    correctAnswer: 1
+    correctAnswer: 1,
+    chosenAnswer: null,
   },
   {
     question: 'Whats the name of the first geezer to talk about the thing in question 2?',
@@ -58,7 +63,8 @@ var quiz = [
       'Friedrich Hasenöhrl',
       'Christian Doppler',
     ],
-    correctAnswer: 2
+    correctAnswer: 2,
+    chosenAnswer: null,
   },
 ]
 
@@ -67,9 +73,55 @@ export default {
   name: 'Quiz',
   data: function() {
     return {
-      quiz: quiz
+      quiz: quiz,
+      picked: '',
     }
-  }
+  },
+  methods: {
+    checkAnswers: function() {
+      var allCorrect = true
+      for (const quizItem of this.quiz)
+      {
+        var questionCorrect = ((quizItem.chosenAnswer !== null) && (quizItem.chosenAnswer.charAt(3) == quizItem.correctAnswer))
+      }
+
+      if (!questionCorrect)
+      {
+        allCorrect = false
+      }
+
+      var goodSound = document.getElementById('goodSound');
+      var badSound = document.getElementById('badSound');
+
+      if(allCorrect)
+      {
+        console.log('Quiz: All correct')
+        goodSound.play()
+      }
+      else
+      {
+        console.log('Quiz: Some not correct')
+        badSound.play()
+      }
+    },
+  },
+  mounted() {
+      var goodSound = document.createElement('audio')
+      goodSound.setAttribute('src', '/quiz-sounds/good.mp3')
+      goodSound.setAttribute('autostart', 'false')
+      goodSound.setAttribute('id', 'goodSound')
+      goodSound.setAttribute('type', 'audio/mp3')
+      
+      document.body.appendChild(goodSound)
+
+      var badSound = document.createElement('audio')
+      badSound.setAttribute('src', '/quiz-sounds/bad.mp3')
+      badSound.setAttribute('autostart', 'false')
+      badSound.setAttribute('id', 'badSound')
+      badSound.setAttribute('type', 'audio/mp3')
+      
+      document.body.appendChild(badSound)
+    },
 }
 </script>
 
